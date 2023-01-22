3028b7c38f2b4ed0edf9da4115fa497f7045b11b
Adam Killam <amkillam@mun.ca>
2023-01-22 18:44:19 -0330

    Minor syntax fix, changes signals to 5-bit vectors

--- CPU Architecture/Instructions/ALU Operations/OR.vhdl
+++ CPU Architecture/Instructions/ALU Operations/OR.vhdl
@@ -2,15 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
  
 entity or is
-  port (
-    input_1    : in  std_logic;
-    input_2    : in  std_logic;
-    or_result : out std_logic
+  or (
+    input_1    : in  std_logic_vector(4 downto 0);
+    input_2    : in  std_logic_vector(4 downto 0);
+    or_result : out std_logic_vector(4 downto 0)
     );
 end or
  
 architecture rtl of or is
-  signal or_gate : std_logic;
+  signal or_gate : std_logic_vector(4 downto 0);
 begin
   or_gate   <= input_1 or input_2;
   or_result <= or_gate;
